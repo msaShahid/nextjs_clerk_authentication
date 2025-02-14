18192120def1eb1ac5d7b75b6f12e7678261c20a
Mohammad Shahid <msa.ansari28@gmail.com>
2025-02-14 23:30:10 +0530

    `Update middleware to protect non-public routes`

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/user-profile"]);
+//const isProtectedRoute = createRouteMatcher(["/user-profile"]);
+const isPublicRoute = createRouteMatcher(["/","/sign-in(.*)","sign-out(.*)"])
+
 
 export default clerkMiddleware( async(auth, req) => {
-  if(isProtectedRoute(req)) await auth.protect();
+ // if(isProtectedRoute(req)) await auth.protect();
+  if(!isPublicRoute(req)) await auth.protect();
 });
 
 export const config = {
